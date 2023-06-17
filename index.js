ab4755749f9b3ac6c94b10c1d459e4761ed4dd13
mary446 <135317707+mary446@users.noreply.github.com>
2023-06-17 10:14:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+      console.log(message);
+    }
+    return messages;
+  }
+  
+  function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  
